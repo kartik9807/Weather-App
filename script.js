d212a565e9e3697a80c624a0ef7cf2cb59b1cb7a
kartik shah <78698460+kartik9807@users.noreply.github.com>
2026-02-13 22:57:01 +0530

    api changed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // API call
 const options = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': 'f00c4fe413msh3fcb9bc36bfc17ep1297f1jsnb5147e0b24ae',
-        'x-rapidapi-host': 'cities-temperature.p.rapidapi.com'
-    }
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'f00c4fe413msh3fcb9bc36bfc17ep1297f1jsnb5147e0b24ae',
+		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+	}
 };
 async function GetWeather(city){
-    const url = `https://cities-temperature.p.rapidapi.com/weather/v1/current?location=${city}`;
+    const url = `https://open-weather13.p.rapidapi.com/city?city=${city}&lang=EN`;
     try {
         const response = await fetch(url, options);
         const result = await response.json();
@@ -41,8 +41,7 @@ document.getElementById("search").addEventListener("click",async function (){
         let a = await fetch("./weatherIconMap.json");
         let response = await a.json();
         for (const key in response) {
-            if((des.toLowerCase()).includes(key)){
-                console.log(response[key]);
+            if(des.includes(key)){
                 return response[key];
             }
         }
@@ -50,11 +49,11 @@ document.getElementById("search").addEventListener("click",async function (){
     }    
 
     // temperature box :- 
-    const maxT = weather.temperature + Math.ceil(Math.random()*2);
-    const minT = weather.temperature - Math.floor((Math.random()*2).toFixed(2));
+    const maxT = Math.floor(weather.main.temp_max - 32 * 5/9 + 1).toFixed(2);
+    const minT = Math.floor(weather.main.temp_min - 32 * 5/9).toFixed(2);
     let text = `<h1 class="head">Temperature</h1>
                 <div class="content">    
-                    <h1>${weather.temperature} <sup>°</sup>C</h1>
+                    <h1>${Math.floor(weather.main.temp - 32 * 5/9).toFixed(2)} <sup>°</sup>C</h1>
                     <h2>Max temperature is ${maxT} <sup>°</sup>C</h2>
                     <h2>Min temperature is ${minT} <sup>°</sup>C</h2>
                 </div>`
@@ -64,18 +63,44 @@ document.getElementById("search").addEventListener("click",async function (){
     let text2 = `<h1 class="head">Humidity Info</h1>
                 <div class="content">
                     <img src="Weather Icons/humidity.svg" alt="" width="100" height="100" alt="icon">
-                    <h1>${weather.humidity}%</h1>
+                    <h1>${weather.main.humidity}%</h1>
                 </div>`
     document.querySelector(".hum").innerHTML = text2;
     
     // wind box :-
+    // let svg = ;
     let text3 = `<h1 class="head">Wind Info</h1>
                 <div class="content">
-                    <img src="Weather Icons/${await Weather_icon(weather.description)}.svg" alt="" width="100" height="100" alt="icon">
-                    <h2>${weather.description}</h2>
-                    <h2>${weather.wind_speed} Km/Hrs</h2>
+                    <img src="Weather Icons/${await Weather_icon(weather.weather[0].description)}.svg" alt="" width="100" height="100" alt="icon">
+                    <h2>${weather.weather[0].description}</h2>
+                    <h2>${weather.wind.speed} Km/Hrs</h2>
                 </div>`
     document.querySelector(".wind").innerHTML = text3;
 });
 
+
+
+// useless api 
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'x-rapidapi-key': 'f00c4fe413msh3fcb9bc36bfc17ep1297f1jsnb5147e0b24ae',
+//         'x-rapidapi-host': 'cities-temperature.p.rapidapi.com'
+//     }
+// };
+// async function GetWeather(city){
+//     const url = `https://cities-temperature.p.rapidapi.com/weather/v1/current?location=${city}`;
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         if(!response.ok){ // to check if data coming or not
+//             throw new Error("Api Unavailable");
+//         }
+//         return(result);
+//     } catch (err) {
+//         console.error(err);
+//         alert("Currently Unavailable");
+//         return null;// null:- nothing is return by this function
+//     }
+// };
 
